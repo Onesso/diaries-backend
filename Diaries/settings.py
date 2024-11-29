0bd936ea823fc71893174fd15b4c5d00cac8ba21
Onesso <odhiambofrank1965@gmail.com>
2024-11-30 01:28:24 +0300

    integrated the mysql db

--- Diaries/settings.py
+++ Diaries/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'Diaries.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Diaries_database',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
