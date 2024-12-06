09ab7b4b66174c05de1223ffce0b5fb00f4838d4
Onesso <odhiambofrank1965@gmail.com>
2024-12-06 23:24:52 +0300

    linked to mysql; isues in put and delete methods

--- Diaries/settings.py
+++ Diaries/settings.py
@@ -38,20 +38,24 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'noteInfo',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders'
     
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
 ]
 
+# APPEND_SLASH = False
 ROOT_URLCONF = 'Diaries.urls'
 
 TEMPLATES = [
@@ -79,7 +83,7 @@ WSGI_APPLICATION = 'Diaries.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'Diaries_database',
+        'NAME': 'blog_database',
         'USER': 'root',
         'PASSWORD': '',
         'HOST': 'localhost',
@@ -107,6 +111,13 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+
+CORS_ALLOWED_ORIGINS = [
+"http://localhost:5173",
+"http://localhost:5173"
+
+]
+
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
@@ -128,3 +139,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
--- noteInfo/admin.py
+++ noteInfo/admin.py
@@ -3,3 +3,11 @@ from .models import Blog
 # Register your models here.
 
 admin.site.register(Blog)
+# from django.contrib import admin
+# from .models import Blog
+# # Register your models here.
+
+# class NoteAdmin(admin.ModelAdmin):
+#     list_display = ["title", "category", "created", "updated"]
+
+# admin.site.register(Blog, NoteAdmin)
